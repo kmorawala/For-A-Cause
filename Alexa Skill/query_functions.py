75436d0e8b1357eb70e3cad91dcc212b8aff0a01
Krishna <morawala@seas.upenn.edu>
2021-03-10 10:37:47 -0600

    query code clean up

--- Alexa Skill/query_functions.py
+++ Alexa Skill/query_functions.py
@@ -5,11 +5,11 @@ import json
 from keys import CarKeys
 import logging
 
-car = CarKeys()
+car = CarKeys()  # storing the access credentials (file is gitignored)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-# 1. Assume the AWS resource role using STS AssumeRole Action
+# Assume the AWS resource role using STS AssumeRole Action
 sts_client = boto3.client('sts')
 assumed_role_object = sts_client.assume_role(RoleArn=car.get_role_creds(),
                                              RoleSessionName="AssumeRoleSession1")
@@ -23,11 +23,11 @@ dynamodb = boto3.resource('dynamodb',
                           )
 
 
-def get_item_count(tableName):
-    # Access table attribute
-    table = dynamodb.Table(tableName)
+def get_item_count(table_name):
+    ''' returns the table count (number of rows) given a table name'''
+    table = dynamodb.Table(table_name)
     logger.info("In get_item_count")
-    logger.info(tableName)
+    logger.info(table_name)
     logger.info("item_count")
     logger.info(table.item_count)
     # return table's item count
@@ -42,36 +42,43 @@ def get_item_count(tableName):
         return 0
 
 
-def query_next_item(id, tableName):
-    table = dynamodb.Table(tableName)
+def query_next_item(id, table_name):
+    ''' return name and mission of a charity given the table name and charity ID'''
+    table = dynamodb.Table(table_name)
     logger.info("In query_next_item")
     logger.info("ID")
     logger.info(id)
-    queryResponse = table.query(
+
+    # query format expected by DynamoDB
+    query_response = table.query(
         KeyConditionExpression=Key('ID').eq(id)
     )
+
     try:
-        if len(queryResponse['Items']) <= 0:
+        if len(query_response['Items']) <= 0:
             logger.info(
                 "Invalid number of items in the DynamoDB databse per In query_next_item")
             return 'unavailable at this moment', 'Please try again later. '
         else:
-            return queryResponse['Items'][0]['Name'], queryResponse['Items'][0]['Mission']
+            return query_response['Items'][0]['Name'], query_response['Items'][0]['Mission']
     except:
         logger.info("Error occured in query_next_item")
         return 'unavailable at this moment', 'Please try again later. '
 
 
-def get_total_contribution(id, tableName):
+def get_total_contribution(id, table_name):
+    ''' returns the total contribution given a table name and charity ID'''
     logger.info("In get_total_contribution")
-    table = dynamodb.Table(tableName)
+    table = dynamodb.Table(table_name)
 
-    queryResponse = table.query(
+    # query format expected by DynamoDB
+    query_response = table.query(
         KeyConditionExpression=Key('ID').eq(id)
     )
+
     try:
-        if queryResponse:
-            return queryResponse['Items'][0]['TotalContribution']
+        if query_response:
+            return query_response['Items'][0]['TotalContribution']
         else:
             logger.info(
                 "get_total_contribution is None in get_total_contribution")
@@ -81,16 +88,19 @@ def get_total_contribution(id, tableName):
         return -1
 
 
-def get_website(id, tableName):
-    logger.info("In get_website")
-    table = dynamodb.Table(tableName)
+def get_website(id, table_name):
+   ''' returns the charity website given a table name and charity ID'''
+     logger.info("In get_website")
+    table = dynamodb.Table(table_name)
 
-    queryResponse = table.query(
+    # query format expected by DynamoDB
+    query_response = table.query(
         KeyConditionExpression=Key('ID').eq(id)
     )
+
     try:
-        if queryResponse:
-            return queryResponse['Items'][0]['Website']
+        if query_response:
+            return query_response['Items'][0]['Website']
         else:
             logger.info("website is None in get_website")
             return 'Not available at the moment'
@@ -99,15 +109,18 @@ def get_website(id, tableName):
         return 'Not available at the moment'
 
 
-def get_tagline(id, tableName):
-    table = dynamodb.Table(tableName)
+def get_tagline(id, table_name):
+   ''' returns the charity tagline given a table name and charity ID'''
+    table = dynamodb.Table(table_name)
 
-    queryResponse = table.query(
+    # query format expected by DynamoDB
+    query_response = table.query(
         KeyConditionExpression=Key('ID').eq(id)
     )
+
     try:
-        if queryResponse:
-            return queryResponse['Items'][0]['Tagline']
+        if query_response:
+            return query_response['Items'][0]['Tagline']
         else:
             logger.info("Tagline is None in get_tagline")
             return 'Not available at the moment'
@@ -116,13 +129,16 @@ def get_tagline(id, tableName):
         return 'Not available at the moment'
 
 
-def update_total_contribution(id, tableName, updated_contribution):
-    table = dynamodb.Table(tableName)
-    queryResponse = table.update_item(
+def update_total_contribution(id, table_name, updated_contribution):
+  ''' updates the total contribution for a charity, given a table name, updated contribution amount and charity ID'''
+    table = dynamodb.Table(table_name)
+
+    # query format expected by DynamoDB
+    query_response = table.update_item(
         Key={'ID': id},
         UpdateExpression="set #TotalContribution = :c",
         ExpressionAttributeNames={"#TotalContribution": "TotalContribution"},
         ExpressionAttributeValues={":c": updated_contribution},
         ReturnValues="UPDATED_NEW"
     )
-    return queryResponse
+    return query_response
